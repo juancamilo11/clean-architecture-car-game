2bfbb0519cc9b91324e2241952b7b1dc1bc044a2
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-11-08 16:07:33 -0500

    Created project with Spring Initilizr

---
+++ src/main/java/co/com/sofkau/cargame/CleanArchitectureCarGameApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofkau.cargame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureCarGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanArchitectureCarGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/sofkau/cargame/CleanArchitectureCarGameApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofkau.cargame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureCarGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
